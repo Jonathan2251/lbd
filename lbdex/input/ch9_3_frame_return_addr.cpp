227c46951b0192a338f6f15cc034ddef14f42145
Jonathan <gamma_chen@yahoo.com.tw>
2015-10-05 19:22:33 +0800

    Add sub-section frameaddress, returnaddress and eh.return support of chapter Function call

---
+++ lbdex/input/ch9_3_frame_return_addr.cpp
@@ -0,0 +1,15 @@
+// clang -target mips-unknown-linux-gnu -c ch9_3_frame_return_addr.cpp -emit-llvm -o ch9_3_frame_return_addr.bc
+// ~/llvm/test/cmake_debug_build/Debug/bin/llc -march=cpu0 -relocation-model=static -filetype=asm ch9_3_frame_return_addr.bc -o -
+
+/// start
+/*int display_frameaddress() {
+  return (int)__builtin_frame_address(0);
+}*/
+
+extern int fn();
+
+int display_returnaddress() {
+  int a = (int)__builtin_return_address(0);
+  fn();
+  return a;
+}
