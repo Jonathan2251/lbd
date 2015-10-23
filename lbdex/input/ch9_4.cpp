9eb7f6c366b7d5a667927ab19f0913817c5990bf
Jonathan <gamma_chen@yahoo.com.tw>
2015-10-23 19:57:45 +0800

    Re-organize testing ch4_2_1.cpp, ch4_2_2.cpp and ch9_4.cpp

--- lbdex/input/ch9_4.cpp
+++ lbdex/input/ch9_4.cpp
@@ -1,5 +1,5 @@
 // clang -c ch9_4.cpp -emit-llvm -o ch9_4.bc // for #include <stdlib.h>
-// clang -target mips-unknown-linux-gnu -c ch9_4.cpp -emit-llvm -o ch9_4.bc // for #include <stdlib.h>
+// clang -target mips-unknown-linux-gnu -c ch9_4.cpp -emit-llvm -o ch9_4.bc
 
 // ~/llvm/test/cmake_debug_build/Debug/bin/llc -march=cpu0 -mcpu=cpu032I -cpu0-s32-calls=false -relocation-model=pic -filetype=asm ch9_4.bc -o -
 // ~/llvm/test/cmake_debug_build/bin/llc -march=cpu0 -mcpu=cpu032I -cpu0-s32-calls=false -relocation-model=pic -filetype=asm ch9_4.bc -o -
@@ -22,12 +22,13 @@ int sum(int x1, int x2, int x3, int x4, int x5, int x6)
 
 int weight_sum(int x1, int x2, int x3, int x4, int x5, int x6)
 {
-//  int *b = (int*)alloca(sizeof(int) * x1);
-  int *b = (int*)__builtin_alloca(sizeof(int) * x1);
-  *b = 1111;
-  int weight = sum(6*x1, x2, x3, x4, 2*x5, x6);
+//  int *b = (int*)alloca(sizeof(int) * 2 * x6);
+  int* b = (int*)__builtin_alloca(sizeof(int) * 2 * x6);
+  int *a = b;
+  *b = x3;
+  int weight = sum(3*x1, x2, x3, x4, 2*x5, x6);
   
-  return weight; 
+  return (weight + (*a)); 
 }
 
 int test_alloc()
