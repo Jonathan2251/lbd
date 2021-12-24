6e110381b8b21bf5bacce7568ffc65ce4359feda
ChungShu <gamma_chen@yahoo.com.tw>
2021-12-24 13:50:23 +0800

    porting-compiler-rt

---
+++ note/builtin_fabs.c
@@ -0,0 +1,15 @@
+// ~/llvm/release/build/bin/clang -emit-llvm -S builtin_fabs.c -o -
+
+// CHECK-LABEL: define{{.*}} void @test_float_builtin_ops
+void test_float_builtin_ops(float F, double D, long double LD) {
+  volatile float resf;
+  volatile double resd;
+  volatile long double resld;
+
+  resf = __builtin_fabsf(F);
+  resd = __builtin_fabs(D);
+  resld = __builtin_fabsl(LD);
+  // CHECK: call float @llvm.fabs.f32(float
+  // CHECK: call double @llvm.fabs.f64(double
+  // CHECK: call x86_fp80 @llvm.fabs.f80(x86_fp80
+}
